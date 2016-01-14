8be0c059987c1aa0ca573d080bd96fa4d41b6298
sashikantasaikia <sashi@dhsk.ac.in>
2016-01-14 08:55:07 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"> 
+	<title>CSS ART</title>
+	<link rel="stylesheet" href="style.css">
+	<!-- Font -->
+	<link href='http://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+	<h1>CSS ART<span> AWESOME</span></h1>
+
+	<div class="logo">
+		<div class="elemen-satu">
+			<div class="elemen-biru"></div>
+		</div>
+		<div class="elemen-dua"></div>
+	</div>
+
+	<footer>
+		© 2014 <a href="https://github.com/sashikantasaikia/cssart/">CSS ART</a>.
+	</footer>
+</html>
